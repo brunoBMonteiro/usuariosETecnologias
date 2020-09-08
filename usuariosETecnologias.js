8a336cd7f8acdac10abde2d22906fdd5e971c9f8
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-09-07 21:56:31 -0300

    segunda parte do desafio 1-3 checar se o usuario usa CSS ok

--- usuariosETecnologias.js
+++ usuariosETecnologias.js
@@ -7,3 +7,19 @@ const usuarios = [
 for (let usuario of usuarios) {
     console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
 }
+
+function checaSeUsuarioUsaCSS(usuario) {
+    // busca por tecnologia
+    for(let tecnologia of usuario.tecnologias) {
+        if (tecnologia == 'CSS') return true
+    }
+    return false
+  }
+
+  for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
+  
+    if (usuarioTrabalhaComCSS) {
+      console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+    }
+  }
